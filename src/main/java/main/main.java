50d938a7e665be32df77bb5c055f12ccb5319502
cl0tCh <screenchok@gmail.com>
2018-10-02 11:32:52 +0200

    DTO

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -14,7 +14,7 @@ import javax.persistence.Persistence;
 public class main {
     
     public static void main(String[] args) {
-//        Persistence.generateSchema("pu", null);
+       Persistence.generateSchema("pu", null);
     }
     
 }
