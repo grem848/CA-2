c13716fb32943b6b65319c66faf1ce3dbb8fe63d
cl0tCh <screenchok@gmail.com>
2018-10-03 11:56:53 +0200

    s

---
+++ src/main/java/DTO/PhoneDTO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+/**
+ *
+ * @author mohammahomarhariri
+ */
+public class PhoneDTO {
+    
+    private Long id;
+    private String phone;
+    private String description;
+
+    public PhoneDTO(String phone, String description) {
+        this.phone = phone;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
